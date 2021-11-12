a248374281cac222e7a12fe7633f732a61b43a22
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2021-11-12 15:53:55 +0100

    feat: add the x button's function #1
    Add the close function to the x button on the modal, on click on this function the modal will be closed.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,17 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelectorAll(".close");
 
-// launch modal event
+// launch & close modal events
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalClose.forEach((cross) => cross.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}

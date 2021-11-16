720fa5f1be4ad928a67228a714101fd421c2e444
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2021-11-16 09:51:32 +0100

    feat: add the form validation #3
    Add the form validation function that used on form submit and display error message if there is one

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,3 +25,40 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+let username = document.getElementById("first");
+let options = document.getElementsByName("location");
+let tos = document.getElementById("checkbox1");
+let birthdate = document.getElementById("birthdate");
+let errorWrapper = document.getElementById("error-container");
+let submit = document.querySelectorAll(".submit-btn");
+submit.forEach((btn) => btn.addEventListener("click", validateForm));
+
+console.log(options[0].checked);
+
+function getOptions() {
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].checked) return true;
+  }
+}
+
+function validateForm() {
+  console.log(getOptions())
+  if (username.value.length < 2) {
+    errorWrapper.style.display = "block";
+    errorWrapper.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    return false;
+  } else if (!getOptions()) {
+    errorWrapper.style.display = "block";
+    errorWrapper.innerHTML = "Vous devez choisir une option.";
+    return false;
+  } else if (tos.checked == false) {
+    errorWrapper.style.display = "block";
+    errorWrapper.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
+    return false;
+  } else if (birthdate.value == "") {
+    errorWrapper.style.display = "block";
+    errorWrapper.innerHTML = "Vous devez entrer votre date de naissance.";
+    return false;
+  }
+}
